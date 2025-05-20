79903940cda0f05ab14688e558d6d842ec849da8
Rhkellz <67848704+Rhkellz@users.noreply.github.com>
2025-05-20 15:30:02 -0500

    Add files via upload

---
+++ PlayerControllerBPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace LCmod1.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        internal static ManualLogSource stepLog;
+        public static int numSteps = 0;
+        [HarmonyPatch("PlayFootstepSound")]
+        [HarmonyPostfix]
+        static void stepCount() {
+            stepLog = BepInEx.Logging.Logger.CreateLogSource("steps");
+            numSteps++;
+            stepLog.LogInfo("steps = " + numSteps);
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx;
+using System.ComponentModel;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using LCmod1.Patches;
+
+namespace LCmod1 {
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class TutorialModBase : BaseUnityPlugin {
+        private const string modGUID = "Rhkellz.LethalSteps";
+        private const string modName = "LethalSteps";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static TutorialModBase Instance;
+
+        internal ManualLogSource mls;
+        void Awake() {
+            if (Instance == null) {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo(modName + " initiated");
+
+            harmony.PatchAll(typeof(TutorialModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+
+    }
+}
